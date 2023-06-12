bae761a65ed557e915eb212d05c5cf6d2e34a1b8
Joel Getzke <joelcode2023@gmail.com>
2023-06-11 21:10:34 -0500

    fetch requests from server side API

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,107 @@
-var APIKey = "2a8dbfffb1da2a8fc04b40b4ac7b5ff2";
 
 
 
-var tableBody = document.getElementById('repo-table');
-var fetchButton = document.getElementById('fetch-button');
 
-function getApi() {
-  // fetch request gets a list of all the repos for the node.js organization
-  var requestUrl = 'https://api.github.com/orgs/nodejs/repos';
-
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
+function getAPI(searchBar) {
+    
+    var APIKey = "2a8dbfffb1da2a8fc04b40b4ac7b5ff2";
+    
+    
+    
+    
+    var APIURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchBar + "&units=imperial&appid="+APIKey;
+    
+    
+    fetch(APIURL)
+    .then(response => { // arrow function ES6 syntax
+        return response.json()
+    }).then(apiWeatherData => {
+        console.log(apiWeatherData)
+        let htmlcontent = `
+        <h3>${apiWeatherData.name}<i class="fa-solid fa-sun" style="color: #ff5f1f;"></i></h3>
+        <p>Temp: ${apiWeatherData.main.temp}°F</p>
+        <i class="fa-solid fa-wind"></i></p>
+        <p>Humidity: ${apiWeatherData.main.humidity} <i class="fa-solid fa-cloud"></i> <i class="fa-solid fa-water"></i></p>
+        `
+        $("#cityData").html(htmlcontent)
     })
-    .then(function (data) {
-      console.log(data)
-      //Loop over the data to generate a table, each table row will have a link to the repo url
-      for (var i = 0; i < data.length; i++) {
-        // Creating elements, tablerow, tabledata, and anchor
-        var createTableRow = document.createElement('tr');
-        var tableData = document.createElement('td');
-        var link = document.createElement('a');
-
-        // Setting the text of link and the href of the link
-        link.textContent = data[i].html_url;
-        link.href = data[i].html_url;
-
-        // Appending the link to the tabledata and then appending the tabledata to the tablerow
-        // The tablerow then gets appended to the tablebody
-        tableData.appendChild(link);
-        createTableRow.appendChild(tableData);
-        tableBody.appendChild(createTableRow);
-      }
-    });
+    
+    
 }
 
-fetchButton.addEventListener('click', getApi);
+
+
+function GetForcastFiveDays(searchBar) {
+    
+    var APIKey = "2a8dbfffb1da2a8fc04b40b4ac7b5ff2";
+    
+    //api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+    
+    
+    var APIURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchBar + "&units=imperial&appid="+APIKey;
+    
+    
+    fetch(APIURL)
+    .then(response => { // arrow function ES6 syntax
+        return response.json()
+    }).then(apiWeatherData => {
+        console.log(apiWeatherData)
+        let htmlcrds = ""
+        for(i= 0; i<apiWeatherData.list.length;i=i+8){  // 24/3 = 8
+            htmlcrds +=`<div>
+      
+          <h3>${apiWeatherData.list[i].name}<i class="fa-solid fa-sun" style="color: #ff5f1f;"></i></h3>
+          <p>Temp: ${apiWeatherData.list[i].main.temp}°F</p>
+         <p>Wind: ${apiWeatherData.list[i].wind.speed} MPH <img src="https://openweathermap.org/img/${apiWeatherData.list[i].weather[0].icon}@2x.png" /><i class="fa-solid fa-wind"></i></p>
+         <p>Humidity: ${apiWeatherData.list[i].main.humidity} <i class="fa-solid fa-cloud"></i> <i class="fa-solid fa-water"></i></p>//
+         </div> `
+        }
+        console.log(htmlcrds);
+         $("#dayForecast").html(htmlcrds) 
+                    
+    })
+} 
+
+        // let htmlcontent = `
+         // <h3>${apiWeatherData.name}<i class="fa-solid fa-sun" style="color: #ff5f1f;"></i></h3>
+         // <p>Temp: ${apiWeatherData.main.temp}°F</p>
+         // <p>Wind: ${apiWeatherData.wind.speed} MPH <img src="https://openweathermap.org/img/${apiWeatherData.weather[0].icon}@2x.png" /><i class="fa-solid fa-wind"></i></p>
+         // <p>Humidity: ${apiWeatherData.main.humidity} <i class="fa-solid fa-cloud"></i> <i class="fa-solid fa-water"></i></p>//
+         // `
+         // $("#cityData").html(htmlcontent) 
+        
+
+        // let htmlcontent = `
+         // <h3>${apiWeatherData.name}<i class="fa-solid fa-sun" style="color: #ff5f1f;"></i></h3>
+         // <p>Temp: ${apiWeatherData.main.temp}°F</p>
+         // <p>Wind: ${apiWeatherData.wind.speed} MPH <img src="https://openweathermap.org/img/${apiWeatherData.weather[0].icon}@2x.png" /><i class="fa-solid fa-wind"></i></p>
+         // <p>Humidity: ${apiWeatherData.main.humidity} <i class="fa-solid fa-cloud"></i> <i class="fa-solid fa-water"></i></p>//
+         // `
+         // $("#cityData").html(htmlcontent) 
+        
+
+       // let htmlcontent = `
+        // <h3>${apiWeatherData.name}<i class="fa-solid fa-sun" style="color: #ff5f1f;"></i></h3>
+        // <p>Temp: ${apiWeatherData.main.temp}°F</p>
+        // <p>Wind: ${apiWeatherData.wind.speed} MPH <img src="https://openweathermap.org/img/${apiWeatherData.weather[0].icon}@2x.png" /><i class="fa-solid fa-wind"></i></p>
+        // <p>Humidity: ${apiWeatherData.main.humidity} <i class="fa-solid fa-cloud"></i> <i class="fa-solid fa-water"></i></p>//
+        // `
+        // $("#cityData").html(htmlcontent)
+
+
+
+
+
+
+
+
+
+var searchButton = $("searchButton")
+
+$("#searchButton").on("click",function(){
+var searchbar = $("#searchBar").val()
+
+console.log(searchbar)
+getAPI(searchbar)
+GetForcastFiveDays(searchbar)
+})
